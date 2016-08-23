b38cdacbf82cffef2ebc5d686647428ed174f1c6
nstone76 <natestone76@gmail.com>
2016-08-23 11:39:17 -0600

    no, really, add the slideshow

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 $(document).ready(function () {
+$(function() {
+  $('#slides').slidesjs({
+      width: 940,
+      height: 528,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+    });
+  });
+
 $('a[href^="#"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
+    var targetId = target.prop('id');
     if( target.length ) {
         event.preventDefault();
-        $('html, body').stop().animate({
+        if()
+        if(targetId == 'photos') {
+            $('html, body').stop().animate({
             scrollTop: target.offset().top,
-        }, 800);
-        $('html, body').animate({
-            scrollTop: '-=200px',
-        }, 400);
+            }, 800);
+        } else {
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top,
+                }, 800);
+            $('html, body').animate({
+                scrollTop: '-=200px',
+            }, 400);
+        };
     }
 });
 });
